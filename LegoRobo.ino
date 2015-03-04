725b7fbdd4af3764252e3064747f1b3f4b6af653
ImRaptor <jharmon@pami.ca>
2015-03-04 08:33:09 -0600

    More work on walking

--- LegoRobo.ino
+++ LegoRobo.ino
@@ -3,23 +3,20 @@
 */
 
 #include <Servo.h>
+#define leftFootPin 11
+#define LFPos  94
+#define leftLegPin  9
+#define LLPos 85
+#define rightFootPin 4
+#define RFPos 99
+#define RLPos 82
+#define rightLegPin 2
 
 Servo leftFoot, leftLeg, rightFoot, rightLeg;
-
-int leftFootPin = 11;
-int LFPos = 94;
-int leftLegPin = 9;
-int LLPos = 87;
-int rightFootPin = 4;
-int RFPos = 99;
-int RLPos = 85;
-int rightLegPin = 2;
-
-
 String inputString = "";         // a string to hold incoming data
 boolean stringComplete = false;  // whether the string is complete
 
-void movePart(Servo tarServ, int* pos, int amount);
+void movePart(Servo tarServ, int amount);
 
 void setup()
 {
@@ -83,9 +80,14 @@ void loop()
       }
     }
     else if (inputString[0] == 'm'){
-      for (int i = 0; i < 6; i++){
-        rightStep();
+      for (byte i = 0; i < 5; i++){
+        forwardStep();
+        //leftStep();
       }
+      movePart(leftLeg, -15);
+      //Serial.print("LL:");
+      //Serial.print(leftLeg.read());
+      //Serial.print('\n');
     }
     
     stringComplete = false; 
@@ -104,33 +106,59 @@ void serialEvent() {
 }
 
 void movePart(Servo tarServ, int amount){
-  
   int direct = 1;
   if (amount < 0){
     direct = -1;
   }
   int hold = tarServ.read();
-  for (int i = 0; i < abs(amount); i++){
-    tarServ.write(hold+(i*direct));
+  for (int i = 0; i <= abs(amount); i++){
+    tarServ.write(hold+(byte(i)*direct));
+    //Serial.print(i);
+    //Serial.print(':');
+    //Serial.print(amount);
+    //Serial.print('\n');
     delay(20);
   }
-  Serial.print(tarServ.read());
-  Serial.print('\n');
+  //Serial.print(tarServ.read());
+  //Serial.print('\n');
   //Serial.print("Done\n");
 }
 
-void rightStep(){
-  movePart(rightFoot,10);      // RF +10 RL +00 LF +00 LL +00
-  movePart(leftFoot,10);       // RF +10 RL +00 LF +10 LL +00
+void forwardStep(){
+  movePart(rightFoot, 10);     // RF +10 RL +00 LF +00 LL +00
+  movePart(leftFoot, 10);      // RF +10 RL +00 LF +10 LL +00
   movePart(rightFoot, 20);     // RF +30 RL +00 LF +10 LL +00
-  movePart(leftFoot,-10);      // RF +30 RL +00 LF +00 LL +00
+  movePart(leftFoot, -10);     // RF +30 RL +00 LF +00 LL +00
   movePart(rightLeg, -15);     // RF +30 RL -15 LF +00 LL +00
+  if (leftLeg.read() != LLPos){
+    movePart(leftLeg, -15);
+  }
   movePart(rightFoot, -30);    // RF +00 RL -15 LF +00 LL +00
   movePart(leftFoot, -10);     // RF +00 RL -15 LF -10 LL +00
-  movePart(rightFoot, -10);    
-  movePart(leftFoot, -20);
-  movePart(rightFoot, 10);
-  movePart(rightLeg, 15);
-  movePart(leftLeg, 15);
-  movePart(leftFoot, 30);
+  movePart(rightFoot, -10);    // RF -10 RL -15 LF -10 LL +00
+  movePart(leftFoot, -20);     // RF -10 RL -15 LF -30 LL +00
+  movePart(rightFoot, 10);     // RF +00 RL -15 LF -30 LL +00
+  movePart(rightLeg, 15);      // RF +00 RL +00 LF -30 LL +00
+  movePart(leftLeg, 15);       // RF +00 RL +00 LF -30 LL +15
+  movePart(leftFoot, 30);      // RF +00 RL +00 LF +00 LL +15
+  //movePart(leftLeg, -15);      // RF +00 RL +00 LF +00 LL +00
 }
+
+/**
+* Unused
+void leftStep(){
+  movePart(leftFoot, -10);      
+  movePart(rightFoot, -10);     
+  movePart(leftFoot, -20);      
+  movePart(rightFoot, 10);    
+  movePart(leftLeg, 15);      
+  movePart(leftFoot, 30);     
+  movePart(rightFoot, 10);    
+  movePart(leftFoot, 10);     
+  movePart(rightFoot, 20);    
+  movePart(leftFoot, -10);     
+  movePart(leftLeg, -15);      
+  movePart(rightLeg, -15);     
+  movePart(rightFoot, -30);    
+  movePart(rightLeg, 15);    
+}**/
